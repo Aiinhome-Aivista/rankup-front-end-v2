04802dc9debf5a176017776f5d3714b56f0cefe7
Pinki Paul <pp3785828@gmail.com>
2026-01-05 19:03:47 +0530

    added

--- src/features/dashboard/components/AssessmentsOverview.tsx
+++ src/features/dashboard/components/AssessmentsOverview.tsx
@@ -79,7 +79,7 @@ const AssessmentsOverview = () => {
                   <button className="rounded-lg p-1.5 transition-colors hover:bg-[#F3F4FF] cursor-pointer"
                   >
                     {item.action === "view" && <Eye size={16} onClick={() => navigate("/teacher/dashboard/review-assessment")} />}
-                    {item.action === "edit" && <Edit2 size={16} />}
+                    {item.action === "edit" && <Edit2 size={16} onClick={() => navigate("/student/attend-assessment/id")} />}
                     {item.action === "share" && <Share2 size={16} />}
                   </button>
                 </td>
